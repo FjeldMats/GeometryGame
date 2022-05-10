26091f63ec2194e96c18ca3dda6443f46db0ed1c
FjeldMats <FjeldMats@gmail.com>
2022-05-10 20:15:28 +0200

    add bullets

---
+++ bullet.py
@@ -0,0 +1,20 @@
+from math import cos, radians, sin
+import pygame
+
+class Bullet: 
+    def __init__(self, x, y, angle, owner):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.velocity = 5
+        self.owner = owner
+        self.hit = False
+    
+    def update(self):
+        self.x += self.velocity * sin(radians(self.angle))  
+        self.y += self.velocity * cos(radians(self.angle))
+
+
+    def display(self, screen):
+        if not self.hit:
+            pygame.draw.circle(screen, (255,255,255), (self.x, self.y), 5)
--- player.py
+++ player.py
@@ -1,11 +1,17 @@
 import pygame
 from math import cos, radians, sin
 
+from bullet import Bullet
+
 class Player: 
     def __init__(self):
         self.angle = 0
         self.angle_velocity = 0
         self.angle_direction = 0 # 0 = no movement
+        self.bullets = []
+
+        self.aim_x = None
+        self.aim_y = None
 
     def movement_event(self, event):
         keys = pygame.key.get_pressed()
@@ -15,6 +21,9 @@ class Player:
             self.angle_direction = -1
         else: 
             self.angle_direction = 0
+
+        if keys[pygame.K_SPACE]:
+            self.bullets.append(Bullet(self.aim_x,self.aim_y,self.angle,self))
         
     def update(self):
         if self.angle_direction == 1:
@@ -31,11 +40,17 @@ class Player:
         width, height = screen.get_size()
 
         center = ((width//2), (height//2))
-        aim_loc = ((center[0] + sin(radians(angle)) * (size*1.5)), (center[1] + cos(radians(angle)) * (size*1.5)))
+        self.aim_x = center[0] + sin(radians(angle)) * (size*1.5)
+        self.aim_y = center[1] + cos(radians(angle)) * (size*1.5)
 
         screen.blit(pygame.font.SysFont("Arial", 10).render(f"angle {angle}", 1, pygame.Color("coral")), (0,20))
 
         pygame.draw.rect(screen, (255,255,255), pygame.Rect((width//2) - (size//2), (height//2) - (size//2), size, size))
 
         # draw aiming line
-        pygame.draw.line(screen, (255,255,255), center, aim_loc, 5)
+        pygame.draw.line(screen, (255,255,255), center, (self.aim_x, self.aim_y), 5)
+
+        for bullet in self.bullets:
+            bullet.update()
+            bullet.display(screen)
+            
